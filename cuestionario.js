a89a94870e14fc1dd68a3d755e6faa059320a6b7
a20150012 <131173403+a20150012@users.noreply.github.com>
2023-09-19 08:46:31 -0500

    Add files via upload

---
+++ cuestionario.js
@@ -0,0 +1,6 @@
+var nombre = prompt("Esta es la Trivia de la primavera. Escribe tu nombre:");
+
+var p1 = prompt("¿Cómo se llama al primer día de la Primavera\A: Primer Día de la Primavera\B: Equinoccio de Primavera\C: Equinoccio de Invierno");
+
+
+}
